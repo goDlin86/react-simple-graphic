c4d99478b2aec9f90cf7c3481748bb0082ef77d7
goDlin <godlin086@gmail.com>
2022-07-08 21:28:59 +0300

    simple graphic ui

--- src/SimpleGraphic.stories.js
+++ src/SimpleGraphic.stories.js
@@ -18,6 +18,5 @@ const Template = (args) => <SimpleGraphic {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  data: [[0, 60], [100, 90], [200, 90], [300, 120], [400, 100], [500, 120]]
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,65 @@
 import React from 'react'
+import { curveNatural, line } from 'd3'
 
+export const SimpleGraphic = ({ data }) => {
+    const viewBoxWidth = 600
+    const viewBoxHeight = 400
+    const paddingX = 6
+    const paddingY = 4
+    const bottomAxisHeight = 30
+    const leftAxisWidth = 50
+    const bodyHeight = viewBoxHeight - bottomAxisHeight - 2 * paddingY
+    const bodyWidth = viewBoxWidth - leftAxisWidth - 2 * paddingX
+    const leftAxis = {
+        pos: {
+            x: paddingX,
+            y: paddingY,
+        },
+        size: {
+            width: leftAxisWidth,
+            height: bodyHeight,
+        },
+    }
+    const bottomAxis = {
+        pos: {
+            x: paddingX + leftAxisWidth,
+            y: paddingY + bodyHeight,
+        },
+        size: {
+            width: bodyWidth,
+            height: bottomAxisHeight,
+        },
+    }
+    const body = {
+        pos: {
+            x: leftAxis.pos.x + leftAxisWidth,
+            y: paddingY,
+        },
+        size: {
+            width: bodyWidth,
+            height: bodyHeight,
+        },
+    }
 
-export const SimpleGraphic = ({ backgroundColor }) => {
-
+    const stepX = bodyWidth / data.length
+    const linePath = line().curve(curveNatural)(data.map((d, i) => [stepX * (i + 1), bodyHeight - d[1]]))
 
     return (
-        <div style={backgroundColor && { backgroundColor }}>TEST</div>
+        <svg width="300" height="200" viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}>
+            <Translate {...body.pos}>
+                <linearGradient id="linear-gradient" x1="0" x2="0" y1="0" y2="1">
+                    <stop offset="0%" stop-color="rgba(89, 188, 142, 0.3)"></stop>
+                    <stop offset="100%" stop-color="rgba(253, 238, 249, 0.3)"></stop>
+                </linearGradient>
+                <path d={linePath + ` L${bodyWidth},${bodyHeight},${stepX},${bodyHeight}Z`} fill="url(#linear-gradient)"/>
+                <path d={linePath} stroke="#59bc8e" fill="none" strokeWidth={4} />
+            </Translate>
+
+        </svg>
     )
+}
+
+const Translate = ({ x=0, y=0, children }) => {
+    if (!x && !y) return children
+    return <g transform={`translate(${x},${y})`}>{children}</g>
 }
